d5f69f3b2424cbfc418a966804875a2182b36e6c
tngh4037 <tngh4037@gmail.com>
2024-10-26 16:07:16 +0900

    section4. 스레드 제어와 생명 주기1 - join - 필요한 상황

--- src/thread/control/join/JoinMainV1.java
+++ src/thread/control/join/JoinMainV1.java
@@ -7,22 +7,46 @@ public class JoinMainV1 {
 
     public static void main(String[] args) {
         log("Start");
-        Thread thread1 = new Thread(new Job(), "thread-1");
-        Thread thread2 = new Thread(new Job(), "thread-2");
 
+        SumTask task1 = new SumTask(1, 50);
+        SumTask task2 = new SumTask(51, 100);
+        Thread thread1 = new Thread(task1, "thread-1");
+        Thread thread2 = new Thread(task2, "thread-2");
         thread1.start();
         thread2.start();
+
+        log("task1.result = " + task1.result);
+        log("task2.result = " + task2.result);
+        int sumAll = task1.result + task2.result;
+        log("task1 + task2 = " + sumAll);
+
         log("End");
     }
 
-    static class Job implements Runnable {
+    static class SumTask implements Runnable {
+
+        private final int startValue;
+        private final int endValue;
+        private int result;
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
 
         @Override
         public void run() {
             log("작업 시작");
-            sleep(2000);
-            log("작업 완료");
+
+            sleep(2000); // 뭔가 처리가 걸린다고 가정
+
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+            result = sum;
+
+            log("작업 완료 result = " + result);
         }
     }
-
 }
