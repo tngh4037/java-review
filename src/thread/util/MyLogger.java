047d370795f3e425a01d759019a8f7670c922604
tngh4037 <tngh4037@gmail.com>
2024-10-26 12:09:13 +0900

    section3. 스레드 생성과 실행 - 로거 만들기

---
+++ src/thread/util/MyLogger.java
@@ -0,0 +1,15 @@
+package thread.util;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public final class MyLogger {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+
+    private MyLogger() {}
+
+    public static void log(Object obj) {
+        String time = LocalTime.now().format(formatter);
+        System.out.printf("%s [%9s] %s\n", time, Thread.currentThread().getName(), obj);
+    }
+}
---
+++ src/thread/util/MyLoggerMain.java
@@ -0,0 +1,11 @@
+package thread.util;
+
+import static thread.util.MyLogger.*;
+
+public class MyLoggerMain {
+
+    public static void main(String[] args) {
+        log("hello thread");
+        log(123456);
+    }
+}
