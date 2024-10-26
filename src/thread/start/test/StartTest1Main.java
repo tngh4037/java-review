3fd97e8fd907550b56861d53d61a6e24be187160
tngh4037 <tngh4037@gmail.com>
2024-10-26 12:45:04 +0900

    section3. 스레드 생성과 실행 - 문제와 풀이

---
+++ src/thread/start/test/StartTest1Main.java
@@ -0,0 +1,27 @@
+package thread.start.test;
+
+import static thread.util.MyLogger.log;
+
+public class StartTest1Main {
+
+    public static void main(String[] args) {
+        CounterThread counterThread = new CounterThread();
+        counterThread.start();
+    }
+
+    static class CounterThread extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 1; i < 6; i++) {
+                log("value: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+}
---
+++ src/thread/start/test/StartTest4Main.java
@@ -0,0 +1,38 @@
+package thread.start.test;
+
+import static thread.util.MyLogger.log;
+
+public class StartTest4Main {
+
+    public static void main(String[] args) {
+        Thread threadA = new Thread(new PrintWork("A", 1000), "Thread-A");
+        Thread threadB = new Thread(new PrintWork("B", 500), "Thread-A");
+        threadA.start();
+        threadB.start();
+    }
+
+    static class PrintWork implements Runnable {
+
+        private final String content;
+        private final long sleepMs;
+
+        public PrintWork(String content, long millis) {
+            this.content = content;
+            this.sleepMs = millis;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                log(content);
+
+                try {
+                    Thread.sleep(sleepMs);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+}
