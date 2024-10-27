d2e6de3ad429d9cb4abdb25a897e9093e7905e6b
tngh4037 <tngh4037@gmail.com>
2024-10-27 11:22:51 +0900

    section5. 스레드 제어와 생명 주기2 - 인터럽트 - 시작2

---
+++ src/thread/control/interrupt/ThreadStopMainV2.java
@@ -0,0 +1,40 @@
+package thread.control.interrupt;
+
+import static thread.util.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+public class ThreadStopMainV2 {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        thread.start();
+
+        sleep(4000);
+        log("작업 중단 지시 thread.interrupt()");
+        thread.interrupt(); // true
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+    }
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+
+            try {
+                while (true) {
+                    log("작업 중");
+                    Thread.sleep(3000);
+                }
+            } catch (InterruptedException e) {
+                // InterruptedException 예외가 터지면서, 인터럽트 상태는 풀림. ( isInterrupted() 는 false 가 됨. )
+                log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted()); // false
+                log("interrupt message=" + e.getMessage());
+                log("state=" + Thread.currentThread().getState());
+            }
+
+            log("자원 정리");
+            log("자원 종료");
+        }
+    }
+}
