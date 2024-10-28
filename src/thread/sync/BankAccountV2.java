c6de9a26c4d77f15cee5a45785c48f88fb4820d4
tngh4037 <tngh4037@gmail.com>
2024-10-28 16:07:57 +0900

    section7. 동기화 - synchronized - synchronized 코드 블럭

--- src/thread/sync/BankAccountV2.java
+++ src/thread/sync/BankAccountV2.java
@@ -33,7 +33,7 @@ public class BankAccountV2 implements BankAccount {
     }
 
     @Override
-    public synchronized int getBalance() {
+    public synchronized int getBalance() { // 참고) balance 값을 수정하는 withdraw() 메서드에 synchronized 가 걸려있기 때문에, balance에 대한 메모리 가시성 문제는 고민하지 않아도 된다. ( 여기서는, 하나의 스레드가 락을 획득하고 작업을 마칠때까지, 다른 스레드가 BankAccount 에서 어떠한 작업을 하지 않도록 하기 위해 synchronized 를 설정했다. 가시성 때문에 getBalance()에 synchronized 를 적용한게 아님. )
         return balance;
     }
 }
