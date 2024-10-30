24cc275adbf02806019c3f631dae7a85a1450861
tngh4037 <tngh4037@gmail.com>
2024-10-30 15:42:37 +0900

    section9. 생산자 소비자 문제1 - 예제1

---
+++ src/thread/bounded/BoundedMain.java
@@ -0,0 +1,77 @@
+package thread.bounded;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static thread.util.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+public class BoundedMain {
+
+    public static void main(String[] args) {
+        // 1. BoundedQueue 선택
+        BoundedQueue queue = new BoundedQueueV1(2);
+
+        // 2. 생선자, 소비자 실행 순서 선택, 반드시 하나만 선택!
+        // producerFirst(queue); // 생산자 먼저 실행
+        consumerFirst(queue); // 소비자 먼저 실행
+    }
+
+    private static void producerFirst(BoundedQueue queue) {
+        log("== [생산자 먼저 실행] 시작, " + queue.getClass().getSimpleName() + " ==");
+        List<Thread> threads = new ArrayList<>();
+
+        startProducer(queue, threads);
+        printAllState(queue, threads);
+
+        startConsumer(queue, threads);
+        printAllState(queue, threads);
+        log("== [생산자 먼저 실행] 종료, " + queue.getClass().getSimpleName() + " ==");
+    }
+
+    private static void startProducer(BoundedQueue queue, List<Thread> threads) {
+        System.out.println();
+        log("생산자 시작");
+        for (int i = 1; i <= 3; i++) {
+            Thread producer = new Thread(new ProducerTask(queue, "data" + i), "producer" + i);
+            threads.add(producer);
+
+            producer.start();
+
+            sleep(100); // 생산자가 순서대로 수행할 수 있도록 하기 위해 sleep 을 걸어주었다. (로그 상 순서대로 보이게)
+        }
+    }
+
+    private static void startConsumer(BoundedQueue queue, List<Thread> threads) {
+        System.out.println();
+        log("소비자 시작");
+        for (int i = 1; i <= 3; i++) {
+            Thread consumer = new Thread(new ConsumerTask(queue), "consumer" + i);
+            threads.add(consumer);
+
+            consumer.start();
+
+            sleep(100); // 소비자가 순서대로 수행할 수 있도록 하기 위해 sleep 을 걸어주었다. (로그 상 순서대로 보이게)
+        }
+    }
+
+    private static void printAllState(BoundedQueue queue, List<Thread> threads) {
+        System.out.println();
+        log("현재 상태 출력, 큐 데이터: " + queue);
+        for (Thread thread : threads) {
+            log(thread.getName() + ": " + thread.getState());
+        }
+    }
+
+    private static void consumerFirst(BoundedQueue queue) {
+        log("== [소비자 먼저 실행] 시작, " + queue.getClass().getSimpleName() + " ==");
+        List<Thread> threads = new ArrayList<>();
+
+        startConsumer(queue, threads);
+        printAllState(queue, threads);
+
+        startProducer(queue, threads);
+        printAllState(queue, threads);
+        log("== [소비자 먼저 실행] 종료, " + queue.getClass().getSimpleName() + " ==");
+    }
+}
