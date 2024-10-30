ad9378ddff3361b951b03ebfa2e3e1c87142d1d5
tngh4037 <tngh4037@gmail.com>
2024-10-30 18:28:30 +0900

    section9. 생산자 소비자 문제1 - 예제3

---
+++ src/thread/bounded/BoundedQueueV3.java
@@ -0,0 +1,61 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import static thread.util.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+public class BoundedQueueV3 implements BoundedQueue {
+
+    private final Queue<String> queue = new ArrayDeque<>();
+    private final int max;
+
+    public BoundedQueueV3(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public synchronized void put(String data) {
+        while (queue.size() == max) {
+            log("[put] 큐가 가득 참, 생산자 대기");
+
+            try {
+                wait(); // RUNNABLE -> WAITING, 모니터 락 반납
+                log("[put] 생산자 깨어남");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        queue.offer(data);
+        log("[put] 생산자 데이터 저장, notify() 호출");
+
+        notify(); // 대기 스레드 깨움 ( WAITING -> BLOCKED )
+    }
+
+    @Override
+    public synchronized String take() {
+        while (queue.isEmpty()) {
+            log("[take] 큐에 데이터가 없음, 소비자 대기");
+            try {
+                wait();
+                log("[take] 소비자 깨어남");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String data = queue.poll();
+        log("[take] 소비자 데이터 획득, notify() 호출");
+
+        notify(); // 대기 스레드 깨움 ( WAITING -> BLOCKED )
+
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+}
