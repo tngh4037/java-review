766da6cbfda67372f66bf3aa746e943f523b3085
tngh4037 <tngh4037@gmail.com>
2024-10-30 16:27:57 +0900

    section9. 생산자 소비자 문제1 - 예제2

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -10,7 +10,8 @@ public class BoundedMain {
 
     public static void main(String[] args) {
         // 1. BoundedQueue 선택
-        BoundedQueue queue = new BoundedQueueV1(2);
+        // BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
         // 2. 생선자, 소비자 실행 순서 선택, 반드시 하나만 선택!
         // producerFirst(queue); // 생산자 먼저 실행
