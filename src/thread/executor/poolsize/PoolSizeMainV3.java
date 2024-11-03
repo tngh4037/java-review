29fdf3a0addcf9ebb0a4d9c950d1907224ed77ac
tngh4037 <tngh4037@gmail.com>
2024-11-04 03:19:44 +0900

    section14. 스레드 풀과 Executor 프레임워크2 - Executor 전략 - 캐시 풀 전략

---
+++ src/thread/executor/poolsize/PoolSizeMainV3.java
@@ -0,0 +1,40 @@
+package thread.executor.poolsize;
+
+import thread.executor.ExecutorUtils;
+import thread.executor.RunnableTask;
+
+import java.util.concurrent.*;
+
+import static thread.util.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+public class PoolSizeMainV3 {
+
+    public static void main(String[] args) {
+        // ExecutorService es = Executors.newCachedThreadPool();
+        ExecutorService es = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+                3, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>());
+
+        log("pool 생성");
+        ExecutorUtils.printState(es);
+
+        for (int i = 1; i <= 4; i++) {
+            String taskName = "task" + i;
+            es.execute(new RunnableTask(taskName));
+            ExecutorUtils.printState(es, taskName);
+        }
+
+        sleep(3000);
+        log("== 작업 수행 완료 ==");
+        ExecutorUtils.printState(es);
+
+        sleep(3000);
+        log("== maximumPoolSize 대기 시간 초과 ==");
+        ExecutorUtils.printState(es);
+
+        es.close();
+        log("== shutdown 완료 ==");
+        ExecutorUtils.printState(es);
+    }
+}
