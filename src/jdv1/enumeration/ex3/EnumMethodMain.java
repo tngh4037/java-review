635cee3aed7da404c6cea17fe42f54438f66d181
tngh4037 <tngh4037@gmail.com>
2024-11-06 04:36:29 +0900

    study: 열거형 - ENUM - 열거형 - 주요 메서드

---
+++ src/jdv1/enumeration/ex3/EnumMethodMain.java
@@ -0,0 +1,21 @@
+package jdv1.enumeration.ex3;
+
+import java.util.Arrays;
+
+public class EnumMethodMain {
+
+    public static void main(String[] args) {
+        Grade[] values = Grade.values();
+        System.out.println("values = " + Arrays.toString(values));
+        System.out.println();
+
+        for (Grade value : values) {
+            System.out.println("name=" + value.name() + ", ordinal=" + value.ordinal());
+        }
+        System.out.println();
+
+        String input = "GOLD";
+        Grade gold = Grade.valueOf(input);
+        System.out.println("gold = " + gold);
+    }
+}
