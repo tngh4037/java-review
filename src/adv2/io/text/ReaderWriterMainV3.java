2285226419cabacb6e101fd03ef680949754ab09
tngh4037 <tngh4037@gmail.com>
2024-11-09 10:54:46 +0900

    study: 문자 다루기3 - Reader, Writer

---
+++ src/adv2/io/text/ReaderWriterMainV3.java
@@ -0,0 +1,31 @@
+package adv2.io.text;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class ReaderWriterMainV3 {
+
+    public static void main(String[] args) throws IOException {
+        String writeString = "ABC";
+        System.out.println("write String: " + writeString);
+
+        // 파일에 쓰기
+        FileWriter fw = new FileWriter("temp/hello.txt", StandardCharsets.UTF_8);
+        fw.write(writeString);
+        fw.close();
+
+        // 파일에서 읽기
+        StringBuilder content = new StringBuilder();
+        FileReader fr = new FileReader("temp/hello.txt", StandardCharsets.UTF_8);
+
+        int ch;
+        while ((ch = fr.read()) != -1) {
+            content.append((char) ch);
+        }
+        fr.close();
+
+        System.out.println("read String = " + content);
+    }
+}
