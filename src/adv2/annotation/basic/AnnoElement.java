627f5986729561f97ead095b54eac881bc05f4bf
tngh4037 <tngh4037@gmail.com>
2024-11-14 07:50:18 +0900

    study: 메타 애노테이션

--- src/adv2/annotation/basic/AnnoElement.java
+++ src/adv2/annotation/basic/AnnoElement.java
@@ -2,10 +2,13 @@ package adv2.annotation.basic;
 
 import adv1.util.MyLogger;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface AnnoElement {
     String value();
     int count() default 0;
