fc95061a4dc04a9233eb00d50bc815ced52603a3
tngh4037 <tngh4037@gmail.com>
2024-11-20 22:14:04 +0900

    study: HTTP 서버1 - 시작

---
+++ src/adv2/was/v1/HttpServerV1.java
@@ -0,0 +1,94 @@
+package adv2.was.v1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+import static adv2.util.MyLogger.log;
+
+public class HttpServerV1 {
+
+    private final int port;
+
+    public HttpServerV1(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        log("서버 시작 port: " + port);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            process(socket);
+        }
+    }
+
+    private void process(Socket socket) throws IOException {
+
+        try (socket;
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), false, StandardCharsets.UTF_8);
+        ) {
+
+            String requestString = requestToString(reader);
+            if (requestString.contains("/favicon.ico")) {
+                log("favicon 요청");
+                return;
+            }
+
+            log("HTTP 요청 정보 출력");
+            System.out.println(requestString);
+
+            log("HTTP 응답 생성 중...");
+            sleep(5000);// 가정) 서버 처리 시간
+            responseToClient(writer);
+            log("HTTP 응답 전달 완료");
+        }
+    }
+
+    private void responseToClient(PrintWriter writer) {
+        // 웹 브라우저에 전달하는 내용
+        String body = "<h1>Hello World</h1>";
+        int length = body.getBytes(StandardCharsets.UTF_8).length;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("HTTP/1.1 200 OK\r\n"); // start line
+        sb.append("Content-Type: text/html\r\n"); // header
+        sb.append("Content-Length: ").append(length).append("\r\n"); // header
+        sb.append("\r\n"); // empty line
+        sb.append(body); // message body
+
+        log("HTTP 응답 정보 출력");
+        System.out.println(sb);
+
+        writer.println(sb);
+        writer.flush();
+    }
+
+    private static String requestToString(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) {
+                break;
+            }
+            sb.append(line).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    private static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/adv2/was/v1/ServerMain.java
@@ -0,0 +1,13 @@
+package adv2.was.v1;
+
+import java.io.IOException;
+
+public class ServerMain {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        HttpServerV1 server = new HttpServerV1(PORT);
+        server.start();
+    }
+}
